80dee19df6ea26fc3c7b1586e5422ac690d27fb3
Pro-Nil <87724453+Pro-Nil@users.noreply.github.com>
2022-02-24 17:11:58 +0530

    Update BedwarsArenaStartMessageBWP.java

--- me/pronil/BedWars/BedwarsArenaStartMessageBWP.java
+++ me/pronil/BedWars/BedwarsArenaStartMessageBWP.java
@@ -1,19 +1,24 @@
 
 package me.pronil.BedWars;
 
+
+import java.io.File;
+
 import com.andrei1058.bedwars.proxy.api.ArenaStatus;
 import com.andrei1058.bedwars.proxy.api.CachedArena;
-import com.andrei1058.bedwars.proxy.api.event.*;
-import java.io.File;
-import me.pronil.API.Reflection;
+import com.andrei1058.bedwars.proxy.api.event.ArenaCacheUpdateEvent;
 import me.pronil.Main;
+import net.md_5.bungee.api.chat.*;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
+
 
 public class BedwarsArenaStartMessageBWP
         implements Listener {
@@ -23,47 +28,55 @@ public class BedwarsArenaStartMessageBWP
     String minPlayersPassedTulip = this.config.getString("minPlayersPassedTulip").replaceAll("&", "§");
     String arenaStartingText = this.config.getString("arenaStartingText").replaceAll("&", "§");
     String arenaStartingTulip = this.config.getString("arenaStartingTulip").replaceAll("&", "§");
+    String sound = this.config.getString("play-sound");
     String lobbyWorld = this.config.getString("lobbyWorldName").trim();
+
 
     @EventHandler
     public void onArenaStart(ArenaCacheUpdateEvent e) {
         CachedArena Arena2 = e.getArena();
         final String ArenaName = Arena2.getArenaName();
         final String ArenaGroup = Arena2.getArenaGroup();
+        final FileConfiguration file = Main.getPlugins().getConfig();
         ArenaStatus ArenaStatus = Arena2.getStatus();
-        final World lobby = Bukkit.getWorld(this.lobbyWorld);
+        final World Lobby = Bukkit.getWorld(this.lobbyWorld);
         if (ArenaStatus == com.andrei1058.bedwars.proxy.api.ArenaStatus.WAITING) {
             if (Arena2.getCurrentPlayers() >= this.config.getInt("minPlayers")) {
-                new BukkitRunnable(){
-
+                new BukkitRunnable() {
                     public void run() {
                         String arena = "";
-                        lobby.getPlayers().forEach(player -> player.sendMessage(" "));
-                        Reflection.JSONMessage joinmsg = new Reflection.JSONMessage("");
-                        Reflection.JSONMessage.ChatExtra addon = new Reflection.JSONMessage.ChatExtra(BedwarsArenaStartMessageBWP.this.minPlayersPassedText.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
-                        addon.addClickEvent(Reflection.JSONMessage.ClickEventType.RUN_COMMAND, "/bw join " + ArenaName.toLowerCase() + arena);
-                        addon.addHoverEvent(Reflection.JSONMessage.HoverEventType.SHOW_TEXT, BedwarsArenaStartMessageBWP.this.minPlayersPassedTulip.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
-                        joinmsg.addExtra(addon);
-                        lobby.getPlayers().forEach(player -> Reflection.sendChatPacket(player, joinmsg.toString()));
-                        lobby.getPlayers().forEach(player -> player.sendMessage(" "));
+                        final TextComponent addon = new TextComponent(BedwarsArenaStartMessageBWP.this.minPlayersPassedText.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
+                        addon.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/bw join " + ArenaName.toLowerCase() + arena));
+                        addon.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, (new ComponentBuilder(minPlayersPassedTulip.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup))).create()));
+                        for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.getWorld() == Lobby)
+                                    player.sendMessage(" ");
+                            player.spigot().sendMessage((BaseComponent)addon);
+                            if (sound.equalsIgnoreCase("true")){
+                                player.playSound(player.getLocation(), Sound.valueOf(file.getString("Sound")), 10.0F, 10.0F);}
+                                    player.sendMessage(" ");
+                                }
                     }
                 }.runTaskLater(Main.getPlugin(), 10L);
             }
-        } else if (ArenaStatus == com.andrei1058.bedwars.proxy.api.ArenaStatus.STARTING) {
+            } else if (ArenaStatus == com.andrei1058.bedwars.proxy.api.ArenaStatus.STARTING) {
             new BukkitRunnable(){
 
                 public void run() {
                     String arena = "";
-                    lobby.getPlayers().forEach(player -> player.sendMessage(" "));
-                    Reflection.JSONMessage joinmsg = new Reflection.JSONMessage("");
-                    Reflection.JSONMessage.ChatExtra addon = new Reflection.JSONMessage.ChatExtra(BedwarsArenaStartMessageBWP.this.arenaStartingText.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
-                    addon.addClickEvent(Reflection.JSONMessage.ClickEventType.RUN_COMMAND, "/bw join " + ArenaName.toLowerCase() + arena);
-                    addon.addHoverEvent(Reflection.JSONMessage.HoverEventType.SHOW_TEXT, BedwarsArenaStartMessageBWP.this.arenaStartingTulip.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
-                    joinmsg.addExtra(addon);
-                    lobby.getPlayers().forEach(player -> Reflection.sendChatPacket(player, joinmsg.toString()));
-                    lobby.getPlayers().forEach(player -> player.sendMessage(" "));
-                }
-            }.runTaskLater(Main.getPlugin(), 10L);
+                    final TextComponent addon = new TextComponent(BedwarsArenaStartMessageBWP.this.arenaStartingText.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup));
+                    addon.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/bw join " + ArenaName.toLowerCase() + arena));
+                    addon.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, (new ComponentBuilder(arenaStartingTulip.replace("{arena}", ArenaName).replace("{arenagroup}", ArenaGroup))).create()));
+
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.getWorld() == Lobby)
+                            player.sendMessage(" ");
+                        player.spigot().sendMessage((BaseComponent)addon);
+                        if (sound.equalsIgnoreCase("true")){
+                        player.playSound(player.getLocation(), Sound.valueOf(file.getString("Sound")), 10.0F, 10.0F);}
+                        player.sendMessage(" ");
+                    }
+                }}.runTaskLater(Main.getPlugin(), 10L);
         }
     }
 }
